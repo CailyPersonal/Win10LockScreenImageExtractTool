877009f9270cbe84ddb946831ef060885cd39943
CailyPersonal <hsqsa@hotmail.com>
2017-03-01 10:07:25 +0800

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Win10LockScreenImageExtractTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Browse = new System.Windows.Forms.Button();
+            this.OK = new System.Windows.Forms.Button();
+            this.textBox_Target = new System.Windows.Forms.TextBox();
+            this.label_Info = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(259, 83);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "浏览";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(349, 46);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 60);
+            this.OK.TabIndex = 2;
+            this.OK.Text = "提取";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // textBox_Target
+            // 
+            this.textBox_Target.Location = new System.Drawing.Point(14, 48);
+            this.textBox_Target.Name = "textBox_Target";
+            this.textBox_Target.Size = new System.Drawing.Size(320, 21);
+            this.textBox_Target.TabIndex = 3;
+            // 
+            // label_Info
+            // 
+            this.label_Info.AutoSize = true;
+            this.label_Info.Location = new System.Drawing.Point(16, 25);
+            this.label_Info.Name = "label_Info";
+            this.label_Info.Size = new System.Drawing.Size(29, 12);
+            this.label_Info.TabIndex = 4;
+            this.label_Info.Text = "信息";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 7);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(411, 10);
+            this.progressBar.TabIndex = 5;
+            this.progressBar.Visible = false;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Checked = true;
+            this.checkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox.Location = new System.Drawing.Point(15, 87);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(120, 16);
+            this.checkBox.TabIndex = 6;
+            this.checkBox.Text = "完成后打开文件夹";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(434, 123);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label_Info);
+            this.Controls.Add(this.textBox_Target);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.Browse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Win 10 锁屏图片提取工具 v1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.TextBox textBox_Target;
+        private System.Windows.Forms.Label label_Info;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.CheckBox checkBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace Win10LockScreenImageExtractTool
+{
+    public partial class MainForm : Form
+    {
+        private delegate void UpdateShowEventHandler(string str, int value);
+        private event UpdateShowEventHandler UpdateShow;
+        private delegate void UpdateProgressEventHandler(bool show);
+        private event UpdateProgressEventHandler UpdateProgress;
+
+        private Thread background;
+        private List<FileInfo> pathes = new List<FileInfo>();
+
+        public MainForm()
+        {
+            if (Environment.OSVersion.Version.Major != 10)
+            {
+                if (DialogResult.OK == MessageBox.Show("此工具仅支持Win10操作系统，其它系统不支持！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error))
+                {
+                    Environment.Exit(0x00);
+                }
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+
+        private void OnUpdateShow(string str, int value)
+        {
+            this.Invoke(new Action(delegate
+                 {
+                     label_Info.Text = str;
+                     progressBar.Value = value;
+                 }));
+        }
+
+        private void OnUpdateProgress(bool show)
+        {
+            this.Invoke(new Action(delegate 
+                {
+                    if (show)//显示进度条，功能不可用
+                    {
+                        progressBar.Visible = true; 
+                        textBox_Target.Enabled = false;
+                        OK.Enabled = false;
+                        Browse.Enabled = false;
+                        checkBox.Enabled = false;
+                    }
+                    else
+                    {
+                        progressBar.Visible = false;
+                        textBox_Target.Enabled = true;
+                        OK.Enabled = true;
+                        Browse.Enabled = true;
+                        checkBox.Enabled = true;
+                    }
+                }));
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            background = new Thread(CheckAndBuildList);
+            background.Start();
+        }
+
+        private void CheckAndBuildList()
+        {
+            UpdateShow += OnUpdateShow;
+            UpdateProgress += OnUpdateProgress;
+            UpdateProgress.Invoke(true);
+
+            // 设置锁屏文件的文件夹路径
+            string Folder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Folder += @"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+
+
+            if (Directory.Exists(Folder))
+            {
+
+                DirectoryInfo folder = new DirectoryInfo(Folder);
+                FileInfo[] files = folder.GetFiles();
+
+                int AllFilesCount = files.Count();
+
+                UpdateShow.Invoke(string.Format("共检测到{0}个文件，准备开始分析", AllFilesCount), 0);
+
+                int index = 0;
+
+                foreach (FileInfo info in files)
+                {
+                    index++;
+                    UpdateShow.Invoke(string.Format("正在分析第{0}个文件...", index), (int)(((double)index * 100) / AllFilesCount));
+                    if (info.Length > 100000)
+                    {
+                        pathes.Add(info);
+                    }
+                }
+
+                UpdateShow.Invoke(string.Format("共检测到{0}个有效文件，请先选择导出文件夹，再点击“导出”进行导出", pathes.Count()), 100);
+                Thread.Sleep(500);
+
+                UpdateProgress.Invoke(false);
+            }
+            else
+            {
+                MessageBox.Show("无法检测到指定路径，可能您的系统尚未打开或不支持该功能，运行错误", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            UpdateShow -= OnUpdateShow;
+            UpdateProgress -= OnUpdateProgress;
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                textBox_Target.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            background = new Thread(ExtractFiles);
+            background.Start();
+        }
+
+
+        private void ExtractFiles()
+        {
+            UpdateShow += OnUpdateShow;
+            UpdateProgress += OnUpdateProgress;
+            UpdateProgress.Invoke(true);
+
+            if (Directory.Exists(textBox_Target.Text))
+            {
+                StreamReader sr;
+                StreamWriter sw;
+
+                UpdateShow.Invoke("开始导出文件", 0);
+
+                string Folder = textBox_Target.Text + "\\";
+                int index = 0;
+                int fail = 0;
+                string TargetPath;
+
+                foreach (FileInfo info in pathes)
+                {
+                    index++;
+                    UpdateShow.Invoke(string.Format("正在提取第{0}个文件，文件大小为：{1}KB", index, info.Length / 1024),
+                        (int)(((double)index * 100) / pathes.Count()));
+
+                    TargetPath = Folder + index.ToString("D8") + ".jpg";
+                    try
+                    {
+                        File.Copy(info.FullName, TargetPath);
+                    }
+                    catch(IOException e)
+                    {
+                        fail++;
+                    }
+                }
+
+                UpdateShow.Invoke(string.Format("成功导出{0}个文件，失败{1}个！", pathes.Count() - fail, fail), 100);
+                    Thread.Sleep(500);
+
+                    if (checkBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("explorer.exe", Folder);
+                    }
+            }
+            else
+            {
+                MessageBox.Show("无法检测到目标路径，运行错误", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            UpdateProgress.Invoke(false);
+
+            UpdateShow += OnUpdateShow;
+            UpdateProgress += OnUpdateProgress;
+        }
+    }
+}
